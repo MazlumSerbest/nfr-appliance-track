4d943e26233565e16e5fb95af071a2fd985c3d4f
MazlumSerbest <mazlumserbest93@gmail.com>
2023-12-05 15:46:33 +0300

    Providers and toaster added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,16 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Providers>{children}</Providers>
+                <Toaster
+                    position="bottom-center"
+                    toastOptions={{
+                        duration: 4000,
+                        className: "text-blue-400",
+                    }}
+                />
+            </body>
         </html>
     );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,16 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { SWRConfig } from "swr";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <NextUIProvider>{children}</NextUIProvider>;
+    return (
+        <SWRConfig
+            value={{
+                // refreshInterval: 3000,
+                fetcher: (url) => fetch(url).then((res) => res.json()),
+            }}
+        >
+            <NextUIProvider>{children}</NextUIProvider>
+        </SWRConfig>
+    );
 }
