537cbc48002bcdf52d7406d19962796d08b4ee89
MazlumSerbest <mazlumserbest93@gmail.com>
2023-12-20 17:23:58 +0300

    New constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,8 @@
+export const userTypes = [
+    { name: "Yönetici", key: "admin" },
+    { name: "Kullanıcı", key: "user" },
+];
+
 export const activeOptions = [
     { name: "Evet", key: "true" },
     { name: "Hayır", key: "false" },
