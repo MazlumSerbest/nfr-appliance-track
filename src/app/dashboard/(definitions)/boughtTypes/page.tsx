11683d161b6b54e197b223a4e2405b931107ed3f
MazlumSerbest <mazlumserbest93@gmail.com>
2024-01-02 10:20:43 +0300

    BoughtType defiinition added

--- src/app/dashboard/(definitions)/boughtTypes/page.tsx
+++ src/app/dashboard/(definitions)/boughtTypes/page.tsx
@@ -24,7 +24,159 @@ import useUserStore from "@/store/user";
 import { activeOptions } from "@/lib/constants";
 import AutoComplete from "@/components/AutoComplete";
 
+interface IFormInput {
+    id: number;
+    type: string;
+    createdBy: string;
+    updatedBy?: string;
+}
+
 export default function BoughtTypes() {
+    const [isNew, setIsNew] = useState(false);
+    const { user: currUser } = useUserStore();
+    const { isOpen, onClose, onOpen, onOpenChange } = useDisclosure();
+
+    const { register, reset, handleSubmit, control } = useForm<IFormInput>({});
+    const onSubmitNew: SubmitHandler<IFormInput> = async (data) => {
+        data.createdBy = currUser?.username ?? "";
+
+        await fetch("/api/boughtType", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then(async (res) => {
+                const result = await res.json();
+                if (res.ok) {
+                    toast.success(result.message);
+                } else {
+                    toast.error(result.message);
+                }
+                return result;
+            })
+            .then(() => {
+                onClose();
+                reset();
+                mutate();
+            });
+    };
+    const onSubmitUpdate: SubmitHandler<IFormInput> = async (data) => {
+        data.updatedBy = currUser?.username ?? "";
+
+        await fetch(`/api/boughtType/${data.id}`, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then(async (res) => {
+                const result = await res.json();
+                if (res.ok) {
+                    toast.success(result.message);
+                } else {
+                    toast.error(result.message);
+                }
+                return result;
+            })
+            .then(() => {
+                onClose();
+                reset();
+                mutate();
+            });
+    };
+
+    const visibleColumns = ["type", "active", "actions"];
+
+    const sort: SortDescriptor = {
+        column: "createdAt",
+        direction: "descending",
+    };
+
+    const columns: Column[] = [
+        {
+            key: "type",
+            name: "Tip",
+            width: 200,
+            searchable: true,
+            sortable: true,
+        },
+        {
+            key: "active",
+            name: "Aktif",
+            width: 80,
+        },
+        {
+            key: "createdBy",
+            name: "Oluşturan Kullanıcı",
+            width: 80,
+        },
+        {
+            key: "createdAt",
+            name: "Oluşturulma Tarihi",
+            width: 150,
+        },
+        {
+            key: "updatedBy",
+            name: "Güncelleyen Kullanıcı",
+            width: 80,
+        },
+        {
+            key: "updatedAt",
+            name: "Güncellenme Tarihi",
+            width: 150,
+        },
+        {
+            key: "actions",
+            name: "Aksiyonlar",
+            width: 100,
+        },
+    ];
+
+    const renderCell = React.useCallback(
+        (boughtType: BoughtType, columnKey: React.Key) => {
+            const cellValue: any =
+                boughtType[columnKey as keyof typeof boughtType];
+
+            switch (columnKey) {
+                case "active":
+                    return <BoolChip value={cellValue} />;
+                case "createdAt":
+                    return <p>{DateTimeFormat(cellValue)}</p>;
+                case "updatedAt":
+                    return <p>{DateTimeFormat(cellValue)}</p>;
+                case "actions":
+                    return (
+                        <div className="flex justify-start items-center gap-2">
+                            <Tooltip
+                                key={boughtType.id + "-edit"}
+                                content="Düzenle"
+                            >
+                                <span className="text-xl text-green-600 active:opacity-50 cursor-pointer">
+                                    <BiEdit
+                                        onClick={() => {
+                                            setIsNew(false);
+                                            reset(boughtType);
+                                            onOpen();
+                                        }}
+                                    />
+                                </span>
+                            </Tooltip>
+                            <Tooltip
+                                key={boughtType.id + "-del"}
+                                content="Sil"
+                            >
+                                <span className="text-xl text-red-600 active:opacity-50 cursor-pointer">
+                                    <BiTrash onClick={() => {}} />
+                                </span>
+                            </Tooltip>
+                        </div>
+                    );
+                default:
+                    return cellValue ? cellValue : "-";
+            }
+        },
+        [onOpen, reset],
+    );
+
     const { data, error, mutate } = useSWR("/api/boughtType");
 
     if (error) return <div>Yükleme Hatası!</div>;
@@ -56,6 +208,67 @@ export default function BoughtTypes() {
                     onOpen();
                 }}
             />
+            <Modal
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                placement="center"
+                backdrop="opaque"
+                shadow="lg"
+                isDismissable={false}
+                scrollBehavior="outside"
+            >
+                <ModalContent>
+                    <ModalHeader className="flex flex-col gap-1 text-zinc-500">
+                        {isNew ? "Yeni Alım Tipi" : "Alım Tipi Güncelle"}
+                    </ModalHeader>
+                    <ModalBody>
+                        <form
+                            action=""
+                            autoComplete="off"
+                            className="flex flex-col gap-3"
+                            onSubmit={handleSubmit(
+                                isNew ? onSubmitNew : onSubmitUpdate,
+                            )}
+                        >
+                            <div>
+                                <label
+                                    htmlFor="type"
+                                    className="block text-sm font-semibold leading-6 text-zinc-500 after:content-['*'] after:ml-0.5 after:text-red-500"
+                                >
+                                    Tip
+                                </label>
+                                <input
+                                    type="text"
+                                    id="type"
+                                    required
+                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-700 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6 outline-none mt-2"
+                                    {...register("type", {
+                                        required: true,
+                                        maxLength: 50,
+                                    })}
+                                />
+                            </div>
+                            <div className="flex flex-row gap-2 mt-4">
+                                <div className="flex-1"></div>
+                                <Button
+                                    color="danger"
+                                    onPress={onClose}
+                                    className="bg-red-600"
+                                >
+                                    Kapat
+                                </Button>
+                                <Button
+                                    type="submit"
+                                    color="success"
+                                    className="text-white bg-green-600"
+                                >
+                                    Kaydet
+                                </Button>
+                            </div>
+                        </form>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
         </>
     );
 }
