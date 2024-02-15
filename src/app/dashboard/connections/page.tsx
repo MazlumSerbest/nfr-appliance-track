9ed749bdd736177369a39636dd7449472cd852a3
MazlumSerbest <mazlumserbest93@gmail.com>
2024-02-15 15:45:27 +0300

    Connections note length changed

--- src/app/dashboard/connections/page.tsx
+++ src/app/dashboard/connections/page.tsx
@@ -28,7 +28,7 @@ interface IFormInput {
     login: string;
     customerId: number;
     password: string;
-    note: string;
+    note?: string;
     createdBy: string;
 }
 
@@ -327,7 +327,7 @@ export default function Connections() {
                                         rows={3}
                                         className="block w-full rounded-md border-0 px-3.5 py-2 text-zinc-700 shadow-sm ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6 outline-none"
                                         {...register("note", {
-                                            maxLength: 400,
+                                            maxLength: 500,
                                         })}
                                     />
                                 </div>
