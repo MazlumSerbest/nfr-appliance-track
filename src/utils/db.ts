101fe51615d1a2c0459fbc21e693115fd89b0bc1
Mazlum Serbest <mazlumserbest93@gmail.com>
2026-01-05 19:12:15 +0300

    chore: update prisma config to use driver adapter

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,15 +1,22 @@
-import { PrismaClient } from '@prisma/client'
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+    return new PrismaClient({ adapter });
+};
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+    var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
